6b30aa2df90a10a53378c71229ee7812f53a2891
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-05-18 10:51:15 +0530

    Add files via upload

---
+++ Screens/ReadScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Text,View,TouchableOpacity,StyleSheet,ScrollView} from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import db from '../config'
+
+
+export default class ReadScreen extends React.Component{
+    constructor(){
+    super();
+    this.state = {
+        search: '',
+        dataSource:'',
+      };
+    }
+    
+      
+
+        retrieveStories = async()=>{
+            const query = await db.collection("Stories").get()
+            query.docs.map((doc)=>{
+                this.setState({
+                    dataSource:[doc.data()]
+                })
+                console.log(doc.data)
+            })
+        }
+    
+    
+
+
+    render(){
+        if(this.state.search !== ''){
+            this.retrieveStories();
+        }
+        return(
+            <ScrollView>
+            <SearchBar
+            placeholder="Search Here..."
+            onChangeText={(text)=>{
+                this.setState({search:text})
+            }}
+            value={this.state.search}
+            style={{marginTop:15}}
+            />
+        {this.state.dataSource.map((data,index)=>{           
+                return(
+        <View key={index} style={{borderBottomWidth:2}}>
+            <Text>{"Book Id: " + data.Title}</Text>
+            <Text>{"Student Id: " + data.Author}</Text>
+        </View>
+            )
+         })}
+            </ScrollView>
+        );
+    }
+}
